8cf5e9748f8dd8eea419b8c82e3e7b4e8e2535b5
David Toca <david.virusfel@gmail.com>
2011-03-20 01:31:34 -0500

    Added solution in c and cpp

--- uva/11244/11244-2.c
+++ uva/11244/11244-2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#define OP 8
+
+
+int dx[]={1,0,1,0,-1,1,-1,-1};
+int dy[]={0,1,1,-1,0,-1,1,-1};
 
 int main()
 {
@@ -23,6 +28,7 @@ memset(m,46,sizeof(m));
 
 for(i=1;i<=r;i++){
 for(j=1;j<=c;j++){
+/*cin>>m[i][j];*/
 m[i][j]=getchar();
 }
 getchar();
@@ -32,15 +38,13 @@ int res=0;
 for(i=1;i<=r;i++){
 for(j=1;j<=c;j++){
 	if(m[i][j]!='*')continue;
-	          if(m[i][j-1]=='*') continue;
-                  if(m[i-1][j-1]=='*') continue;
-                  if(m[i-1][j]=='*') continue;
-                  if(m[i-1][j+1]=='*') continue;
-                  if(m[i][j+1]=='*') continue;
-                  if(m[i+1][j+1]=='*') continue;
-                  if(m[i+1][j]=='*') continue;
-                  if(m[i+1][j-1]=='*') continue;
-
+	int isStar=1;
+	for(k=0;k<OP;k++)
+		if(m[i+dx[k]][j+dy[k]]=='*'){
+		isStar=0;
+		break;
+		}
+	if(isStar)
 	res++;
 	
 }
--- uva/11244/11244.cpp
+++ uva/11244/11244.cpp
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
 #include <string.h>
+
+using namespace std;
 
 int main()
 {
@@ -13,22 +16,23 @@ scanf("%d %d",&r,&c);
 if(r==0 && c==0)
 break;
 
-getchar();
-
 char m[r+2][c+2];
 
-int i,j,k;
+int i,j;
 
-memset(m,46,sizeof(m));
+/*memset(m,46,sizeof(m));*/
 
-for(i=1;i<=r;i++){
-for(j=1;j<=c;j++){
-m[i][j]=getchar();
-}
-getchar();
-}
+for(i=0;i<r+2;i++)
+for(j=0;j<c+2;j++)
+m[i][j]='.';
+
+for(i=1;i<=r;i++)
+for(j=1;j<=c;j++)
+cin>>m[i][j];
+
 
 int res=0;
+
 for(i=1;i<=r;i++){
 for(j=1;j<=c;j++){
 	if(m[i][j]!='*')continue;
@@ -40,9 +44,7 @@ for(j=1;j<=c;j++){
                   if(m[i+1][j+1]=='*') continue;
                   if(m[i+1][j]=='*') continue;
                   if(m[i+1][j-1]=='*') continue;
-
 	res++;
-	
 }
 
 }
