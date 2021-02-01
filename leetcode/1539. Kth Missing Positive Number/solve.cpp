1f9e8f12e6b26a5b57736161f80d26aee6184642
David Toca <david.virusfel@gmail.com>
2021-02-01 17:40:31 -0500

    add solution for 1539

---
+++ leetcode/1539. Kth Missing Positive Number/solve.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int response = 0;
+        //k=1;
+        for(int i=1, j=0; k!= 0; i++){
+            
+            if(j >= arr.size() || i != arr[j]){
+                response = i;
+                k-=1;
+            } else {
+                j+=1;
+            }
+            
+        }
+        return response;
+
+    }
+};
